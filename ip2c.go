9981d50907b073c7f64bf0db4cb32588fc039385
Kentaro Kawano <kentaro.kawano@gmail.com>
2013-10-18 18:50:01 +0900

    整理中

--- ip2c.go
+++ ip2c.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"github.com/nranchev/go-libGeoIP"
@@ -8,32 +9,56 @@ import (
 	"strings"
 )
 
-var (
-	dbPath    string
-	ipAddress string
-)
+type Config struct {
+	dbPath string
+}
 
-func init() {
-	maxmindDbPath := os.Getenv("MAXMIND_DB_PATH")
-
-	if maxmindDbPath == "" {
-		currentDir, _ := os.Getwd()
-		maxmindDbPath = strings.Join([]string{currentDir, "GeoIP.dat"}, "/")
+func Configure() (*Config, error) {
+	if flag.NArg() == 0 {
+		return nil, errors.New("usage: ip2c [--db db-file-path] ipaddress")
 	}
 
-	flag.StringVar(&dbPath, "db", maxmindDbPath, "Maxmind db file path")
+	geoIpDbPath := os.Getenv("GEOIP_DB_PATH")
+
+	if geoIpDbPath == "" {
+		currentDir, _ := os.Getwd()
+		geoIpDbPath = strings.Join([]string{currentDir, "GeoIP.dat"}, "/")
+	}
+
+	dbPath := flag.String("db", geoIpDbPath, "GeoIP.dat file path")
 	flag.Parse()
+
+	return &Config{*dbPath}, nil
+}
+
+func detectCountry(ipAddress string) string {
+	return ""
+}
+
+func readFromArg() {
+
+}
+
+func readFromStdin() {
+
 }
 
 func main() {
-	if flag.NArg() == 0 {
-		fmt.Fprintln(os.Stderr, "usage: ip2c [--db db-file-path] ipaddress")
+	config, err := Configure()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
+	}
+
+	if flag.NArg() == 0 {
+		readFromStdin()
+	} else {
+		readFromArg()
 	}
 
 	ipAddress := flag.Arg(0)
 
-	geoIp, err := libgeo.Load(dbPath)
+	geoIp, err := libgeo.Load(config.dbPath)
 	if err != nil {
 		fmt.Printf("Error: %s\n", err.Error())
 		return
