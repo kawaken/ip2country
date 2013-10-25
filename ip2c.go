9bdccc2fc49740801a23039a1a4b510f567aed6e
Kentaro Kawano <kentaro.kawano@gmail.com>
2013-10-25 18:08:13 +0900

    標準入力に対応

--- ip2c.go
+++ ip2c.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"errors"
+	//"errors"
+	"bufio"
 	"flag"
 	"fmt"
 	"github.com/nranchev/go-libGeoIP"
@@ -9,15 +10,21 @@ import (
 	"strings"
 )
 
+func usage() {
+	fmt.Fprint(os.Stderr, "usage: ip2c [-db db-file-path] ipaddress\n")
+
+	for _, v := range []string{"db", "help"} {
+		f := flag.Lookup(v)
+		fmt.Fprintf(os.Stderr, "  -%s\t: %s\n", v, f.Usage)
+	}
+	os.Exit(1)
+}
+
 type Config struct {
 	dbPath string
 }
 
-func Configure() (*Config, error) {
-	if flag.NArg() == 0 {
-		return nil, errors.New("usage: ip2c [--db db-file-path] ipaddress")
-	}
-
+func Configure() *Config {
 	geoIpDbPath := os.Getenv("GEOIP_DB_PATH")
 
 	if geoIpDbPath == "" {
@@ -25,45 +32,20 @@ func Configure() (*Config, error) {
 		geoIpDbPath = strings.Join([]string{currentDir, "GeoIP.dat"}, "/")
 	}
 
-	dbPath := flag.String("db", geoIpDbPath, "GeoIP.dat file path")
+	dbPath := flag.String("db", geoIpDbPath, "File path of 'GeoIP.dat'")
+	help := flag.Bool("help", false, "Show this message")
+
+	flag.Usage = usage
 	flag.Parse()
 
-	return &Config{*dbPath}, nil
-}
-
-func detectCountry(ipAddress string) string {
-	return ""
-}
-
-func readFromArg() {
-
-}
-
-func readFromStdin() {
-
-}
-
-func main() {
-	config, err := Configure()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+	if *help {
+		flag.Usage()
 	}
 
-	if flag.NArg() == 0 {
-		readFromStdin()
-	} else {
-		readFromArg()
-	}
+	return &Config{*dbPath}
+}
 
-	ipAddress := flag.Arg(0)
-
-	geoIp, err := libgeo.Load(config.dbPath)
-	if err != nil {
-		fmt.Printf("Error: %s\n", err.Error())
-		return
-	}
-
+func detectCountry(geoIp *libgeo.GeoIP, ipAddress string) {
 	loc := geoIp.GetLocationByIP(ipAddress)
 	if loc == nil {
 		fmt.Println("")
@@ -71,4 +53,37 @@ func main() {
 	}
 
 	fmt.Println(loc.CountryName)
+}
+
+func readFromStdin(geoIp *libgeo.GeoIP) {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		detectCountry(geoIp, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+}
+
+func readFromArg(geoIp *libgeo.GeoIP) {
+
+	for _, ipAddress := range flag.Args() {
+		detectCountry(geoIp, ipAddress)
+	}
+}
+
+func main() {
+	config := Configure()
+
+	geoIp, err := libgeo.Load(config.dbPath)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err.Error())
+		return
+	}
+
+	if flag.NArg() == 0 {
+		readFromStdin(geoIp)
+	} else {
+		readFromArg(geoIp)
+	}
 }
